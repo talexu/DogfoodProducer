ba53c2041e85deb0740a0f094ab274d6a6cd5db2
许雲龙 <yxuao@connect.ust.hk>
2018-08-06 23:19:56 +0800

    Adding backend interface

--- app/controllers/dogfood_factory_controller.rb
+++ app/controllers/dogfood_factory_controller.rb
@@ -1,4 +1,55 @@
 class DogfoodFactoryController < ApplicationController
+  before_action :prepare
+
+  class << self
+    attr_accessor :status
+    attr_accessor :progress
+  end
+
   def index
+  end
+
+  def reset
+    self.class.status = 'ready'
+    self.class.progress = 0
+    render_status
+  end
+
+  def start
+    self.class.status = 'start'
+    self.class.progress = 0
+    render_status
+  end
+
+  def increase
+    if self.class.status == 'start'
+      self.class.progress = [100, self.class.progress + 10].min
+    end
+
+    if self.class.progress >= 100
+      self.class.status = 'success'
+    end
+
+    render_status
+  end
+
+  def status
+    render_status
+  end
+
+  private
+
+  def render_status
+    render(
+      json: {
+        status: self.class.status,
+        progress: self.class.progress
+      }
+    )
+  end
+
+  def prepare
+    self.class.status = self.class.status || 'ready'
+    self.class.progress = self.class.progress || 0
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,13 @@ Rails.application.routes.draw do
   get 'dogfood_factory/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'dogfood_factory#index'
+
+  resources :dogfood_factory do
+    collection do
+      put :reset
+      put :start
+      post :increase
+      get :status
+    end
+  end
 end
